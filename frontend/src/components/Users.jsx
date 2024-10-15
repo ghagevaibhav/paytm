d2cddf365fa4327bfaba9d885da9d3e714cf54d8
ghagevaibhav <ghagevaibhav@gmail.com>
2024-10-15 12:52:47 +0530

    signup and debounced filter done

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,37 +1,50 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "./Button"
-import axios from 'axios'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export const Users = () => {
-
-    const [users ,setUsers] = useState([{
-        firstName: "Harkirat",
-        lastName: "Singh",
-        _id: 1
-    }])
+    // Replace with backend call
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
+    const [debouncedFilter, setDebouncedFilter] = useState(filter);
 
     useEffect(() => {
-        (async () => {
-            const res = await axios.get('http://localhost:3000/api/v1/user/bulk')
-            setUsers(res.data)
-        })()
-    }, [])
+        const handler = setTimeout(() => {
+            setDebouncedFilter(filter)
+        }, 400);
 
-    return <div className="flex">
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [filter])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + debouncedFilter)
+            .then(response => {
+                setUsers(response.data.user)
+            })
+    }, [debouncedFilter])
+
+    return <>
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
         <div className="my-2">
-            <input type="text" placeholder="Search Users ..." className="w-full px-2 py-1 border rounded border-slate-200"/>
+            <input onChange={(e) => {
+                setFilter(e.target.value)
+            }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
-            {users.map((user) => <User key={user._id} user={user} />)}
+            {users.map(user => <User user={user} />)}
         </div>
-    </div>
+    </>
 }
 
-function User ({user}){
+function User({user}) {
+    const navigate = useNavigate();
+
     return <div className="flex justify-between">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
@@ -39,15 +52,17 @@ function User ({user}){
                     {user.firstName[0]}
                 </div>
             </div>
-
-            <div className="flex flex-col justify-center h-full">
+            <div className="flex flex-col justify-center h-ful">
                 <div>
                     {user.firstName} {user.lastName}
                 </div>
             </div>
         </div>
-        <div className="flex flex-col justify-center h-full">
-            <Button label={"sendMoney"} />
+
+        <div className="flex flex-col justify-center h-ful">
+            <Button onClick={(e) => {
+                navigate("/send?id=" + user._id + "&name=" + user.firstName);
+            }} label={"Send Money"} />
         </div>
     </div>
 }
